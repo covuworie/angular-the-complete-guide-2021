766f3344f3439e263670875cbdfc897aaadbb3c1
covuworie <chuk.ovuworie@gmail.com>
2021-02-08 14:39:54 -0600

    Add FormsModule and HTTPClientModule

--- src/01/12/my-first-app/src/app/app.module.ts
+++ src/01/12/my-first-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
