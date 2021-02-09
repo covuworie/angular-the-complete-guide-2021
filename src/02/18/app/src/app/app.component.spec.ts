0ea89e138265f572b9a9dccb96b487f5efdc6c97
covuworie <chuk.ovuworie@gmail.com>
2021-02-09 12:55:48 -0600

    Update my-first app to app

--- src/02/18/app/src/app/app.component.spec.ts
+++ src/02/18/app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-first-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
   });
 });
--- src/02/21/app/e2e/src/app.e2e-spec.ts
+++ src/02/21/app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('my-first-app app is running!');
+    expect(await page.getTitleText()).toEqual('app app is running!');
   });
 
   afterEach(async () => {
