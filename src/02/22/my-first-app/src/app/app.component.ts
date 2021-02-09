5423fda1aa9cfd6c8c80cd11075176295aa73857
covuworie <chuk.ovuworie@gmail.com>
2021-02-09 12:42:38 -0600

    Add 22. Working with Component Styles

---
+++ src/02/22/my-first-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+  styles: [
+    `
+      h3 {
+        color: dodgerblue;
+      }
+    `,
+  ],
+})
+export class AppComponent {}
---
+++ src/02/22/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
