b9f33164e6aab7240ef2fe675808c78902b19dd5
covuworie <chuk.ovuworie@gmail.com>
2021-02-09 12:20:07 -0600

    Add 20. Creating Components with the CLI & Nesting Components

---
+++ src/02/20/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
