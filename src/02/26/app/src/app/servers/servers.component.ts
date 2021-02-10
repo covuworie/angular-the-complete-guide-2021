6a5f8e81d175acbe703409ebffc67cec61ec6e09
covuworie <chuk.ovuworie@gmail.com>
2021-02-10 12:08:51 -0600

    Add 26. String Interpolation

---
+++ src/02/26/app/src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  template: ` <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
