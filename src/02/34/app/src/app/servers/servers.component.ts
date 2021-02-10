3083503220e418b14bb3a691e790a0a13019d3ef
covuworie <chuk.ovuworie@gmail.com>
2021-02-10 14:40:07 -0600

    Add 34. Combining all Forms of Databinding

---
+++ src/02/34/app/src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  // template: ` <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test server';
+
+  constructor() {
+    setTimeout(() => (this.allowNewServer = true), 2000);
+  }
+
+  ngOnInit(): void {}
+
+  public onCreateServer() {
+    this.serverCreationStatus = `Server '${this.serverName}' was created!`;
+  }
+
+  public onUpdateServerName(event: InputEvent) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+}
