64f4725f23a780bd07ede4254d5775665519bc40
covuworie <chuk.ovuworie@gmail.com>
2021-02-19 13:05:54 -0600

    Add 52. Creating a 'Recipe' Model

---
+++ src/03/52/app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
