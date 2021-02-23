a865ad67cb2362e9393d082b6f26248e2fc9e790
covuworie <chuk.ovuworie@gmail.com>
2021-02-23 11:36:34 -0600

    Add 65. Splitting Apps into Components

---
+++ src/05/65/app/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
+})
+export class CockpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+
+  public onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+}
---
+++ src/05/65/app/src/app/server-element/server-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
