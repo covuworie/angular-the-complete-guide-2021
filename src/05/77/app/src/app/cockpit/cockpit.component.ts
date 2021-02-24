bf80fcf1d0fb02a3cde76f9a6d3a5d11a2cb2cbc
covuworie <chuk.ovuworie@gmail.com>
2021-02-24 13:26:17 -0600

    Add 77. Projecting Content into Components with ng-content

---
+++ src/05/77/app/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,48 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{
+    name: string;
+    content: string;
+  }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
+    name: string;
+    content: string;
+  }>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentElement', { static: true })
+  serverContentElement: ElementRef;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public onAddServer(serverNameElement: HTMLInputElement) {
+    this.serverCreated.emit({
+      name: serverNameElement.value,
+      content: (this.serverContentElement.nativeElement as HTMLInputElement)
+        .value,
+    });
+  }
+
+  public onAddBlueprint(serverNameElement: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      name: serverNameElement.value,
+      content: (this.serverContentElement.nativeElement as HTMLInputElement)
+        .value,
+    });
+  }
+}
