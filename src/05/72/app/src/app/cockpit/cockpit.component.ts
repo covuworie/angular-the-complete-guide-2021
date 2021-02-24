88584d33cba68c4499251eaaef9ee7243fba00f2
covuworie <chuk.ovuworie@gmail.com>
2021-02-24 11:54:13 -0600

    Add 72. Understanding View Encapsulation

---
+++ src/05/72/app/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css'],
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{ name: string; content: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ name: string; content: string }>();
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public onAddServer() {
+    this.serverCreated.emit({
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+
+  public onAddBlueprint() {
+    this.blueprintCreated.emit({
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+}
