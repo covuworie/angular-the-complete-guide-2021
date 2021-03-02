22433289caa624f23ad1bc0092902a08af8bdd78
covuworie <chuk.ovuworie@gmail.com>
2021-03-02 12:57:52 -0600

    Add 99. Binding to Directive Properties

---
+++ src/07/99/app/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+@Directive({
+  selector: "[appBasicHighlight]",
+})
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.background = "green";
+  }
+}
