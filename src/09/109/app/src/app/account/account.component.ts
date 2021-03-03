746b70373cd2c2da0f404333571e73d9aab8eec8
covuworie <chuk.ovuworie@gmail.com>
2021-03-03 14:18:48 -0600

    Add Accounts Service to Providers Array

--- src/09/109/app/src/app/account/account.component.ts
+++ src/09/109/app/src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import { LoggingService } from "../logging.service";
   selector: "app-account",
   templateUrl: "./account.component.html",
   styleUrls: ["./account.component.css"],
-  providers: [LoggingService],
+  providers: [LoggingService, AccountsService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
