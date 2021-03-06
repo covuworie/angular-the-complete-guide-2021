a77ac06f8c5c06d23e884d736167e6e54f429474
covuworie <chuk.ovuworie@gmail.com>
2021-03-06 17:14:04 -0600

    Add 117. Setting up the Services

---
+++ src/10/117/app/src/app/recipes/recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css'],
+})
+export class RecipesComponent implements OnInit {
+  @Input() selectedRecipe: Recipe;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
