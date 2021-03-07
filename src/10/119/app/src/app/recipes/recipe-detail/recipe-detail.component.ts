c7a0ca6e3928550e56c8d6e3434778eed9c06bbb
covuworie <chuk.ovuworie@gmail.com>
2021-03-06 19:10:21 -0600

    Add 119. Using a Service for Cross-Component Communication

---
+++ src/10/119/app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css'],
+})
+export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
