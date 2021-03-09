9d7ddfd5374768b16aab9a7a9b58b8bfed1f8651
covuworie <chuk.ovuworie@gmail.com>
2021-03-09 12:06:37 -0600

    Add 128. Navigating with Router Links

---
+++ src/11/128/app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "users", component: UsersComponent },
+  { path: "servers", component: ServersComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [ServersService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
