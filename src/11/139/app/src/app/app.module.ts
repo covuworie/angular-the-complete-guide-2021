649b4780d56ac8ffce588394796258a17f55c69f
covuworie <chuk.ovuworie@gmail.com>
2021-03-10 12:38:49 -0600

    Add 139. Practising and Some Common Gotchas

---
+++ src/11/139/app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "users", component: UsersComponent },
+  { path: "users/:id/:name", component: UserComponent },
+  { path: "servers", component: ServersComponent },
+  { path: "servers/:id", component: ServerComponent },
+  { path: "servers/:id/edit", component: EditServerComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  providers: [ServersService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
