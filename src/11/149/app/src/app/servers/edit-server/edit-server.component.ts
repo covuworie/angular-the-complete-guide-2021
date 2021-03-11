bd82506469912b70cf7b05496ca7b2df71014cec
covuworie <chuk.ovuworie@gmail.com>
2021-03-11 13:44:43 -0600

    Add 149. Using a Fake Auth Service

---
+++ src/11/149/app/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,44 @@
+import { query } from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+
+import { ServersService } from "../servers.service";
+
+@Component({
+  selector: "app-edit-server",
+  templateUrl: "./edit-server.component.html",
+  styleUrls: ["./edit-server.component.css"],
+})
+export class EditServerComponent implements OnInit {
+  server: { id: number; name: string; status: string };
+  serverName = "";
+  serverStatus = "";
+  allowEdit = false;
+
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // Works as long as params do not change in component after the initial loading
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    // Observables that are reactive to changes in query params and fragments
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
+    });
+    this.route.fragment.subscribe();
+
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
+  }
+}
---
+++ src/11/149/app/src/app/servers/server/server.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+
+import { ServersService } from "../servers.service";
+
+@Component({
+  selector: "app-server",
+  templateUrl: "./server.component.html",
+  styleUrls: ["./server.component.css"],
+})
+export class ServerComponent implements OnInit {
+  server: { id: number; name: string; status: string };
+
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params["id"];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params["id"]);
+    });
+  }
+
+  onEdit() {
+    this.router.navigate(["edit"], {
+      relativeTo: this.route,
+      queryParamsHandling: "preserve",
+    });
+  }
+}
