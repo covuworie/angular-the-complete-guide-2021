a6a4625190b49efd303c9a4f431e0a97f6ed2d2b
covuworie <chuk.ovuworie@gmail.com>
2021-03-12 13:19:25 -0600

    Add 153. Understanding Location Strategies

---
+++ src/11/153/app/src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id/:name", component: UserComponent }],
+  },
+  {
+    path: "servers",
+    //canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
+      {
+        path: ":id",
+        component: ServerComponent,
+        resolve: { server: ServerResolver },
+      },
+      {
+        path: ":id/edit",
+        canDeactivate: [CanDeactivateGuard],
+        component: EditServerComponent,
+      },
+    ],
+  },
+  // { path: "not-found", component: PageNotFoundComponent },
+  {
+    path: "not-found",
+    component: ErrorPageComponent,
+    data: { message: "Page not found!" },
+  },
+  // wildcard that matches everything else and must be the last route
+  { path: "**", redirectTo: "/not-found" },
+];
+
+@NgModule({
+  // imports: [RouterModule.forRoot(appRoutes, { useHash: true })],  // use hash form for paths
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
