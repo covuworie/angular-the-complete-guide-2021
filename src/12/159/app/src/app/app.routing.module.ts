f215fc36c0153d48d8ff2221075759e0dcb6e41c
covuworie <chuk.ovuworie@gmail.com>
2021-03-13 12:20:03 -0600

    Add 159. Fixing Page Reload Issues

---
+++ src/12/159/app/src/app/app.routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/12/159/app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css'],
+})
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.recipeService = this.recipeService;
+  }
+
+  public onRecipeSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
+}
