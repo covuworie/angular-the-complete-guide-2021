2557560188f072c5c62603bd3b05150323081f49
covuworie <chuk.ovuworie@gmail.com>
2021-03-16 14:10:49 -0500

    Add 174. Errors & Completion

---
+++ src/13/174/app/src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import {
+  interval,
+  Subscription,
+  Observable,
+  observable,
+  Subscriber,
+} from "rxjs";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObservableSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    // this.firstObservableSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const intervalObservable = new Observable(
+      (observer: Subscriber<number>) => {
+        let count = 0;
+        setInterval(() => {
+          observer.next(count);
+          if (count === 5) {
+            observer.complete();
+          }
+          if (count > 3) {
+            observer.error(new Error("Count is greater than 3!"));
+          }
+          count++;
+        }, 1000);
+      }
+    );
+
+    this.firstObservableSubscription = intervalObservable.subscribe(
+      (count) => {
+        console.log(count);
+      },
+      (error: Error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.firstObservableSubscription.unsubscribe();
+  }
+}
