bf71160ed5ad988358884a175db3250478f1173d
covuworie <chuk.ovuworie@gmail.com>
2021-03-18 12:36:31 -0500

    Add 194. TD: Set Default Values with ngModel Property Binding

---
+++ src/15/194/app/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild } from "@angular/core";
+import { NgForm, NgModel } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  @ViewChild("form") form: NgForm;
+  defaultQuestion = "teacher";
+
+  suggestUserName() {
+    const suggestedName = "Superuser";
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
