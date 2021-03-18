85e29b4959cac1f4dd6930fbd6a542af51dcd576
covuworie <chuk.ovuworie@gmail.com>
2021-03-18 13:23:56 -0500

    Add 196. TD: Grouping Form Controls

---
+++ src/15/196/app/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewChild } from "@angular/core";
+import { NgForm, NgModel } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  @ViewChild("form") form: NgForm;
+  defaultQuestion = "teacher";
+  answer = "";
+
+  suggestUserName() {
+    const suggestedName = "Superuser";
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
