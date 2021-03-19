7c133d4ace38d91413b59312136bb0ce69a545b2
covuworie <chuk.ovuworie@gmail.com>
2021-03-19 13:16:37 -0500

    Add 200. TD: Resetting Forms

---
+++ src/15/200/app/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, ViewChild } from "@angular/core";
+import { NgForm, NgModelGroup } from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  @ViewChild("form") form: NgForm;
+  @ViewChild("userData") userData: NgModelGroup;
+  defaultQuestion = "teacher";
+  answer = "";
+  genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = "Superuser";
+    // Overwrite all values at once (not best in this scenario)
+    // this.form.setValue({
+    //   userData: { username: suggestedName, email: "" },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+    // Overwrite only some values
+    this.form.form.patchValue({ userData: { username: suggestedName } });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.userData.value.username;
+    this.user.email = this.userData.value.email;
+    this.user.secretQuestion = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+
+    // Reset the form
+    this.form.reset();
+    // This does almost the same but also resets the submitted status
+    // this.form.resetForm();
+  }
+}
