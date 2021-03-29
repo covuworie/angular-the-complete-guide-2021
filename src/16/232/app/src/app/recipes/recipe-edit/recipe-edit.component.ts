1bfcd1df2a7d19a5270f762c8e174be316944213
covuworie <chuk.ovuworie@gmail.com>
2021-03-29 14:45:08 -0500

    Add 232. Validating User Input

---
+++ src/16/232/app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css'],
+})
+export class RecipeEditComponent implements OnInit {
+  id?: number;
+  editMode = false;
+  recipeForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.editMode = params['id'] != null;
+      if (this.editMode) {
+        this.id = +params['id'];
+      }
+      this.initForm();
+    });
+  }
+
+  get ingredient() {
+    return this.recipeForm.get('ingredients') as FormArray;
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  onAddIngredient() {
+    (this.recipeForm.get('ingredients') as FormArray).push(
+      new FormGroup({
+        name: new FormControl('', Validators.required),
+        amount: new FormControl('', [Validators.required, Validators.min(1)]),
+      })
+    );
+  }
+
+  private initForm() {
+    const recipe = this.recipeService.getRecipe(this.id);
+    let recipeIngredients = new FormArray([]);
+    if (this.editMode && recipe.ingredients) {
+      for (let ingredient of recipe.ingredients) {
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(ingredient.name, Validators.required),
+            amount: new FormControl(ingredient.amount, [
+              Validators.required,
+              // No need for regex as Max does in the video
+              // Validators.pattern(/^[1-9]+[0-9]*$/),
+              Validators.min(1),
+            ]),
+          })
+        );
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(
+        this.editMode ? recipe.name : '',
+        Validators.required
+      ),
+      imagePath: new FormControl(
+        this.editMode ? recipe.imagePath : '',
+        Validators.required
+      ),
+      description: new FormControl(
+        this.editMode ? recipe.description : '',
+        Validators.required
+      ),
+      ingredients: recipeIngredients,
+    });
+  }
+}
