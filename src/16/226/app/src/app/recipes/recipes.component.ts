3203184da5141bfc5bf1ee5841975999c37a4d66
covuworie <chuk.ovuworie@gmail.com>
2021-03-29 12:42:23 -0500

    Add 226. Creating the Template for the (Reactive) Recipe Edit Form

---
+++ src/16/226/app/src/app/recipes/recipes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService],
+})
+export class RecipesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
