76d992ff280adf23a19b7cfa3625e93ea0e547c1
covuworie <chuk.ovuworie@gmail.com>
2021-04-03 18:23:50 -0500

    Add 256. GETting Data

---
+++ src/18/256/app/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post(
+        "https://ng-complete-guide-b5850-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => console.log(responseData));
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        "https://ng-complete-guide-b5850-default-rtdb.firebaseio.com/posts.json"
+      )
+      .subscribe((posts) => console.log(posts));
+  }
+}
