5b7ca6935138e3c95ba4430833463ab8ff1e4d65
covuworie <chuk.ovuworie@gmail.com>
2021-04-05 13:15:59 -0500

    Add 261. Using a Service for HTTP Requests

---
+++ src/18/261/app/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Post } from "./post.model";
+import { PostsService } from "./posts.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.postsService.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postsService.createPost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.postsService.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
---
+++ src/18/261/app/src/app/posts.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PostsService {
+  constructor(private http: HttpClient) {}
+
+  createPost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
+        "https://ng-complete-guide-b5850-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => console.log(responseData));
+  }
+
+  fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        "https://ng-complete-guide-b5850-default-rtdb.firebaseio.com/posts.json"
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {});
+  }
+}
