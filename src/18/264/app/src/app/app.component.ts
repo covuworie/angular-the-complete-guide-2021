ef26821ce7a8e5a81d615f936d1514fcf1395028
covuworie <chuk.ovuworie@gmail.com>
2021-04-05 14:46:12 -0500

    Add 264. Handling Errors

---
+++ src/18/264/app/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Post } from "./post.model";
+import { PostsService } from "./posts.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = "";
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postsService.createPost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postsService.deletePosts().subscribe(() => (this.loadedPosts = []));
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error: HttpErrorResponse) => {
+        // In the Firebase project go to Realtime Database -> Rules and set the value of
+        // the ".read" key to false to initiate this error
+        this.error = error.message;
+        console.log(error);
+      }
+    );
+  }
+}
---
+++ src/18/264/app/src/app/posts.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PostsService {
+  private static readonly url =
+    "https://ng-complete-guide-b5850-default-rtdb.firebaseio.com/posts.json";
+
+  constructor(private http: HttpClient) {}
+
+  createPost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(PostsService.url, postData)
+      // Notice here we subscribe to the result of the request. Normally we would
+      // do this in the AppComponent. We only do it here as this is a rare care
+      // where we do not care about the response in the AppComponent.
+      .subscribe((responseData) => console.log(responseData));
+  }
+
+  fetchPosts() {
+    // Notice difference here from the previous lesson:
+    // Return the Observable. HTTP request is not sent at this point since
+    // subscribe is not called here. It is now called in the AppComponent.
+    return this.http.get<{ [key: string]: Post }>(PostsService.url).pipe(
+      map((responseData) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      })
+    );
+  }
+
+  deletePosts() {
+    return this.http.delete(PostsService.url);
+  }
+}
