2ad577a4628d6254c5eebed9acb2c178842eff37
covuworie <chuk.ovuworie@gmail.com>
2021-04-09 12:59:46 -0500

    Add 281. Storing Recipes

---
+++ src/19/281/app/src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+})
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+}
---
+++ src/19/281/app/src/app/shared/data-storage.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataStorageService {
+  private static readonly baseUrl =
+    'https://ng-course-recipe-book-bce51-default-rtdb.firebaseio.com';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.Recipes;
+    this.http
+      .put<Recipe[]>(`${DataStorageService.baseUrl}/recipes.json`, recipes)
+      .subscribe((response) => console.log(response));
+  }
+}
