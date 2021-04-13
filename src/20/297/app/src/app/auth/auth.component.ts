9298c8dcd4bebb1165c6a2802a17cbe9d6d22b25
covuworie <chuk.ovuworie@gmail.com>
2021-04-13 12:41:11 -0500

    Add 297. Sending Login Requests

---
+++ src/20/297/app/src/app/auth/auth.component.ts
@@ -0,0 +1,60 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {
+  SignUpResponseData,
+  AuthService,
+  LoginResponseData,
+} from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+})
+export class AuthComponent {
+  @ViewChild('authForm') authForm: NgForm;
+  isLoginMode = true;
+  isLoading = false;
+  error = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    if (!this.authForm.valid) {
+      return;
+    }
+
+    const email = this.authForm.value.email;
+    const password = this.authForm.value.password;
+
+    let authObservable:
+      | Observable<LoginResponseData | HttpErrorResponse>
+      | Observable<SignUpResponseData | HttpErrorResponse>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      authObservable = this.authService.login(email, password);
+    } else {
+      authObservable = this.authService.signup(email, password);
+    }
+
+    authObservable.subscribe(
+      (responseData: LoginResponseData | SignUpResponseData) => {
+        console.log(responseData);
+        this.isLoading = false;
+      },
+      (errorMessage: string) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    this.authForm.reset();
+  }
+}
