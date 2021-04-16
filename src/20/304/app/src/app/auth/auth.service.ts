71c4912d9150a468b957c91515f9d9c570ab955e
covuworie <chuk.ovuworie@gmail.com>
2021-04-16 13:37:54 -0500

    Add 304. Adding Auto-Login

---
+++ src/20/304/app/src/app/auth/auth.service.ts
@@ -0,0 +1,138 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { FIREBASE_API_KEY } from './auth-secrets.service';
+import { User } from './user.model';
+
+export interface SignUpResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+export interface LoginResponseData extends SignUpResponseData {
+  registered: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private static readonly signUpUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`;
+  private static readonly loginUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`;
+  // Like Subject but allows to retrieve the previous value without having to
+  // subscribe at the time the value was emitted
+  user = new BehaviorSubject<User>(null);
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<SignUpResponseData | HttpErrorResponse>(AuthService.signUpUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData: SignUpResponseData) => {
+          this.handleAuthentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<LoginResponseData | HttpErrorResponse>(AuthService.loginUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData: LoginResponseData) => {
+          this.handleAuthentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
+        })
+      );
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
+  logout() {
+    this.user.next(null);
+    // There is only one login in auth component, however there are multiple
+    // ways of logging out (logout button, token expiring etc.) so we do this
+    // redirect in this service rather than a component
+    this.router.navigate(['/auth']);
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const tokenExpirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
+    );
+    const user = new User(email, userId, token, tokenExpirationDate);
+    this.user.next(user);
+    // Persist the user to local storage so that it survives session restarts
+    localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+    return throwError(errorMessage);
+  }
+}
