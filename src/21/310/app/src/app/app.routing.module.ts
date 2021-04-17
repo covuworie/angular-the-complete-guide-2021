db3b3364d38f4afb18cd603c56c46e4c4e42a736
covuworie <chuk.ovuworie@gmail.com>
2021-04-17 12:36:20 -0500

    Add 310. Adding an Alert Modal Component

---
+++ src/21/310/app/src/app/app.routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuardService } from './auth/auth.guard.service';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipesResolver } from './recipes/recipe-resolvers.service';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    canActivate: [AuthGuardService],
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipesResolver],
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolver],
+      },
+    ],
+  },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/21/310/app/src/app/shared/alert/alert.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css'],
+})
+export class AlertComponent {
+  @Input() message: string;
+}
