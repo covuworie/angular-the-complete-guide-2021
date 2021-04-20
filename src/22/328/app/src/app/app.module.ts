16f2f21c7c65b7ba50e3a843b0a7f388efa4a223
covuworie <chuk.ovuworie@gmail.com>
2021-04-20 13:43:47 -0500

    Add 328. Understanding the Core Module

---
+++ src/22/328/app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app.routing.module';
+import { AuthComponent } from './auth/auth.component';
+import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+
+@NgModule({
+  declarations: [AppComponent, AuthComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RecipesModule,
+    ShoppingListModule,
+    SharedModule,
+    CoreModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
