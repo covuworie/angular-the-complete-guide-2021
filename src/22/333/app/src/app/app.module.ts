ef6df0308e39c4356aa53bcc60b5b223a46a4ce9
covuworie <chuk.ovuworie@gmail.com>
2021-04-21 13:26:45 -0500

    Add 333. Preloading Lazy-Loaded Code

---
+++ src/22/333/app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app.routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
