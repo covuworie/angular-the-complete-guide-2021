d8a67bdce0777368136ae9963336b5331bcc9031
covuworie <chuk.ovuworie@gmail.com>
2021-04-28 12:23:31 -0500

    Add 358. Removing Redundant Component State Management

---
+++ src/24/358/app/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LoggingService } from '../logging.service';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
+
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private loggingService: LoggingService,
+    private store: Store<fromShoppingList.AppState>
+  ) {}
+
+  ngOnInit(): void {
+    this.ingredients = this.store.select('shoppingList');
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
+  }
+
+  onEdit(id: number) {
+    this.store.dispatch(new ShoppingListActions.StartEdit(id));
+  }
+}
---
+++ src/24/358/app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,70 @@
+import { Ingredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
+
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientId: number;
+}
+
+export interface AppState {
+  shoppingList: State;
+}
+
+const initialState: State = {
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
+  editedIngredient: null,
+  editedIngredientId: -1,
+};
+
+export function shoppingListReducer(
+  state: State = initialState,
+  action: ShoppingListActions.ShoppingListActions
+) {
+  // Immutable updates
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.ingredients],
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[state.editedIngredientId];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.ingredient,
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[state.editedIngredientId] = updatedIngredient;
+
+      return {
+        ...state,
+        ingredients: updatedIngredients,
+        editedIngredient: initialState.editedIngredient,
+        editedIngredientId: initialState.editedIngredientId,
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ingredient, id) => {
+          return id !== state.editedIngredientId;
+        }),
+        editedIngredient: initialState.editedIngredient,
+        editedIngredientId: initialState.editedIngredientId,
+      };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientId: action.id,
+        editedIngredient: { ...state.ingredients[action.id] },
+      };
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: initialState.editedIngredient,
+        editedIngredientId: initialState.editedIngredientId,
+      };
+    default:
+      return state;
+  }
+}
