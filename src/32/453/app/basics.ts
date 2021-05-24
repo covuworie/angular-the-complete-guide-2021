7f5115e35ee9c5196eb9b87277ed29cc649e98f0
covuworie <chuk.ovuworie@gmail.com>
2021-05-24 12:56:52 -0500

    Add 453. Type Inference

---
+++ src/32/453/app/basics.ts
@@ -0,0 +1,48 @@
+// Primitives: number, string, boolean
+// More complex types: array, objects
+// Function types, parameters
+
+// Primitives
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: { name: string; age: number };
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: { name: string; age: number }[];
+
+people = [
+  {
+    name: "Max",
+    age: 32,
+  },
+  {
+    name: "Pete",
+    age: 32,
+  },
+];
+
+// Type inference
+
+let course = "React - The Complete Guide";
+
+// course = 12341;
