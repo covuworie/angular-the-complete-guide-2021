f58a61af42a89d0b9620a9ab2010635f0268c2d6
covuworie <chuk.ovuworie@gmail.com>
2021-05-24 12:42:20 -0500

    Add 451. Base Types & Primitives

---
+++ src/32/451/app/basics.ts
@@ -0,0 +1,16 @@
+// Primitives: number, string, boolean
+// More complex types: array, objects
+// Function types, parameters
+
+// Primitives
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
