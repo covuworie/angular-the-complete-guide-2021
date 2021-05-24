5b4d8a67a0e5897da8619a382038031a8f87a1eb
covuworie <chuk.ovuworie@gmail.com>
2021-05-24 13:52:28 -0500

    Add 457. Understanding Generics

---
+++ src/32/457/app/basics.ts
@@ -0,0 +1,74 @@
+// Primitives: number, string, boolean
+// More complex types: array, objects
+// Function types, parameters
+
+// Primitives
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+type Person = { name: string; age: number }; // Type alias
+
+let person: Person;
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: Person[];
+
+people = [
+  {
+    name: "Max",
+    age: 32,
+  },
+  {
+    name: "Pete",
+    age: 32,
+  },
+];
+
+// Type inference and union types
+
+let course: string | number = "React - The Complete Guide";
+
+course = 12341;
+
+// Functions & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function logToConsole(value: any) {
+  console.log(value);
+}
+
+// Generics (type safe yet flexible as they work with any type)
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, 0); // [0, 1, 2, 3]
+const stringArray = insertAtBeginning(["b", "c", "d"], "a");
+
+// updatedArray[0].split('');
